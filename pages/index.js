dfc44ac7c98971ba1da0d7aac36bfbb548dc60f2
Chris <chris.ware.dev@gmail.com>
2021-09-02 21:10:18 -0700

    Project 1 Initial Setup

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next world</h1>
+    <div>
+      <h1>Hello Next World!</h1>
     </div>
   )
 }
